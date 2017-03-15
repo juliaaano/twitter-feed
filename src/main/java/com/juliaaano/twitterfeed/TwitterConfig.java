bca5abde11c1edbff448b0e1cfad4d7e7bb2c575
juliaaano <juliaaano@gmail.com>
2017-03-15 23:55:41 +1100

    Configure twitter track keywords and poll delay by env variables

--- src/main/java/com/juliaaano/twitterfeed/TwitterConfig.java
+++ src/main/java/com/juliaaano/twitterfeed/TwitterConfig.java
@@ -4,8 +4,17 @@ import static java.lang.System.getenv;
 
 class TwitterConfig {
 
+    static final String TWITTER_POLL_DELAY = env("TWITTER_POLL_DELAY", "10000");
+    static final String TWITTER_TRACK_KEYWORD = env("TWITTER_TRACK_KEYWORD", "juliaaano");
+
     static final String TWITTER_CONSUMER_KEY = getenv("TWITTER_CONSUMER_KEY");
     static final String TWITTER_CONSUMER_SECRET = getenv("TWITTER_CONSUMER_SECRET");
     static final String TWITTER_ACCESS_TOKEN = getenv("TWITTER_ACCESS_TOKEN");
     static final String TWITTER_ACCESS_TOKEN_SECRET = getenv("TWITTER_ACCESS_TOKEN_SECRET");
+
+    private static String env(final String key, final String defaultValue) {
+
+        final String value = System.getenv(key);
+        return value != null ? value : defaultValue;
+    }
 }
--- src/main/java/com/juliaaano/twitterfeed/TwitterFeedRouteBuilder.java
+++ src/main/java/com/juliaaano/twitterfeed/TwitterFeedRouteBuilder.java
@@ -2,6 +2,8 @@ package com.juliaaano.twitterfeed;
 
 import org.apache.camel.builder.RouteBuilder;
 
+import static com.juliaaano.twitterfeed.TwitterConfig.TWITTER_POLL_DELAY;
+import static com.juliaaano.twitterfeed.TwitterConfig.TWITTER_TRACK_KEYWORD;
 import static org.apache.camel.LoggingLevel.INFO;
 
 public class TwitterFeedRouteBuilder extends RouteBuilder {
@@ -9,7 +11,7 @@ public class TwitterFeedRouteBuilder extends RouteBuilder {
     @Override
     public void configure() {
 
-        fromF("twitter://streaming/filter?type=polling&delay=%s&keywords=%s", "10000", "#POTUS")
+        fromF("twitter://streaming/filter?type=polling&delay=%s&keywords=%s", TWITTER_POLL_DELAY, TWITTER_TRACK_KEYWORD)
             .routeId("twitter-polling-consumer")
             .routeDescription("Polls tweets with a given keyword using the Twitter Streaming API.")
             .to("seda:tweets");
